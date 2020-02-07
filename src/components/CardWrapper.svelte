a0d2d33ecd56b36e921b699b47c2ba5548ee6583
Glory Wong Zhaohui <glorywong1001@gmail.com>
2020-02-08 02:07:24 +0800

    add city data

---
+++ src/components/CardWrapper.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let title = ''
+  export let des = ''
+</script>
+
+<main class="card">
+  {#if !!title}
+  <header class="card__header">
+    <div class="card__header-title">{title}</div>
+    <div class="card__header-des">{des}</div>
+  </header>
+  {/if}
+  <article class="card__article">
+    <slot></slot>
+  </article>
+</main>
+
+<style lang="scss">
+  .card {
+    overflow: hidden;
+    border-radius: 2px;
+    background-color: white;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+    padding: 24px;
+
+    &__header {
+      &-title {
+        font-size: 25px; 
+      }
+      &-des {
+        margin-top: 10px;
+        font-size: 15px;
+        color: #999;
+      }
+    }
+
+    &__article {
+      margin-top: 20px;
+    }
+  }
+</style>
