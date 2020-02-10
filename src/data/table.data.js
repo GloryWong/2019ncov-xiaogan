9875b1eb887a9df5940ab60d618b790106556b8a
Glory Wong Zhaohui <glorywong1001@gmail.com>
2020-02-10 18:15:22 +0800

    update count

--- src/data/table.data.js
+++ src/data/table.data.js
@@ -1,8 +1,8 @@
 const table = {
   titles: ['地区', '确诊', '治愈', '死亡'],
-  uptoTime: '2020年2月8日24时',
-  traceCount: 9761,
-  observeCount: 7090
+  uptoTime: '2020年2月9日24时',
+  traceCount: 11727,
+  observeCount: 7921
 };
 
 export default table;
